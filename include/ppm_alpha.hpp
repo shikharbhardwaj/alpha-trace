26dea471daad92782d028944fff7f5147a5e5977
Shikhar Bhardwaj <shikharbhardwaj68@gmail.com>
2016-04-14 22:57:23 +0530

    alpha::ppm_image changes
    Uses a colourspace

--- include/ppm_alpha.hpp
+++ include/ppm_alpha.hpp
@@ -17,17 +17,21 @@
 #include <vector>
 #include <math_alpha.hpp>
 namespace alpha {
+enum class ppm_colours {
+
+};
 class ppm_image {
   private:
     using rgb = alpha::Vec3<int>;
-    std::string name;
     size_t height, width;
+    int col_space;
+    std::string name;
     std::vector<std::vector<rgb>> content;
 
   public:
     ppm_image() = delete;
-    ppm_image(const std::string &nm, size_t h, size_t w)
-        : name(nm), height(h), width(w) {
+    ppm_image(const std::string &nm, size_t h, size_t w, int space = 255)
+        : height(h), width(w), col_space(space), name(nm) {
         content.resize(h);
         for (auto &elem : content) {
             elem.assign(w, rgb(0, 0, 0));
@@ -36,10 +40,7 @@ class ppm_image {
     void set(size_t x, size_t y, rgb color) { content[y][x] = color; }
     void dump() {
         auto file_h = fopen(name.c_str(), "w");
-        std::string header = "P6 ";
-        header +=
-            std::to_string(width) + " " + std::to_string(height) + " 255 ";
-        fprintf(file_h, "%s", header.c_str());
+        fprintf(file_h, "P6\n%lu\n%lu\n%d\n", width, height, col_space);
         for (auto &row : content) {
             for (auto elem : row) {
                 fprintf(file_h, "%c%c%c", elem.x, elem.y, elem.z);
