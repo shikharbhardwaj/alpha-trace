7724adb43e4d3284a27a34eaab29d5072b350aa7
Shikhar Bhardwaj <shikharbhardwaj68@gmail.com>
2016-04-22 23:59:14 +0530

    Rasteriser benchmark

--- include/rasteriser_alpha.hpp
+++ include/rasteriser_alpha.hpp
@@ -50,7 +50,7 @@ class Rasteriser {
     std::unique_ptr<Framebuffer> buf;
     uint32_t screen_width, screen_height;
     float edge_function(const Point &a, const Point &b, const Point &c) {
-        return ((b.y - a.y) * (c.x - a.x) - (b.x - a.x) * (c.y - a.y));
+        return (c.x * (a.y - b.y) + c.y * (b.x - a.x) + a.x * b.y - a.y - b.x);
     }
     std::pair<Point, Point> compute_bounding_box(const Point &a, const Point &b,
                                                  const Point &c) {
@@ -77,23 +77,33 @@ class Rasteriser {
     void draw_triangle(const Point &v0, const Point &v1, const Point &v2) {
         auto bbox = compute_bounding_box(v0, v1, v2);
         auto total_area = edge_function(v0, v1, v2);
-        std::cout << "Total area : " << total_area;
         // Computing the barycentric co-ordinates for colour contribution
-        for (uint32_t i = bbox.first.y; i <= bbox.second.y; i++) {
-            for (uint32_t j = bbox.first.x; j <= bbox.second.x; j++) {
-                Point p(j + 0.5f, i + 0.5f);
-                float w0 = edge_function(v1, v2, p);
-                float w1 = edge_function(v2, v0, p);
-                float w2 = edge_function(v0, v1, p);
-                // If p is visible, render
-                if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
-                    w0 /= total_area;
-                    w1 /= total_area;
-                    w2 /= total_area;
-                    buf->set(j, i, rgb(w0 * buf->col_space, w1 * buf->col_space,
-                                       w2 * buf->col_space));
+        // Triangle setup
+        float a01 = v0.y - v1.y, b01 = v1.x - v0.x;
+        float a12 = v1.y - v2.y, b12 = v2.x - v1.x;
+        float a20 = v2.y - v0.y, b20 = v0.x - v2.x;
+        Point p(bbox.first.x, bbox.first.y);
+        float w0_row = edge_function(v1, v2, p);
+        float w1_row = edge_function(v2, v0, p);
+        float w2_row = edge_function(v0, v1, p);
+        for (; p.y <= bbox.second.y; p.y++) {
+            float w0 = w0_row;
+            float w1 = w1_row;
+            float w2 = w2_row;
+            for (p.x = bbox.first.x; p.x <= bbox.second.x; p.x++) {
+                if ((w1 | w2 | w3) >= 0) {
+                    // If p is visible, render
+                    buf->set(p.x, p.y, rgb(w0 * buf->col_space / total_area,
+                                           w1 * buf->col_space / total_area,
+                                           w2 * buf->col_space / total_area));
                 }
+                w0 += a12;
+                w1 += a20;
+                w2 += a01;
             }
+            w0_row += b12;
+            w1_row += b20;
+            w2_row += b01;
         }
     }
 };
--- spike/code/rasterisation.cpp
+++ spike/code/rasterisation.cpp
@@ -2,8 +2,8 @@
 #include <rasteriser_alpha.hpp>
 int main() {
     const int width = 1366, height = 768;
-    alpha::Vec2f v0 = {width, 0};
-    alpha::Vec2f v1 = {0, 0};
+    alpha::Vec2f v0 = {0, 0};
+    alpha::Vec2f v1 = {width, 0};
     alpha::Vec2f v2 = {width, height};
     alpha::Rasteriser rast(width, height);
     rast.draw_triangle(v0, v1, v2);
