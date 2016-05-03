8378a21720dc7f7acf5fb238fd4e76ec30a99819
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2016-05-03 23:39:03 +0530

    Rasteriser code refactor

--- include/camera_alpha.hpp
+++ include/camera_alpha.hpp
@@ -16,70 +16,81 @@
 #include <iostream>
 #include <math_alpha.hpp>
 namespace alpha {
-namespace camera {
 using namespace math;
-static const float inch_to_mm = 25.4;
 enum class fit_resolution_gate { Fill = 0, Overscan };
-void compute_screen_coordinates(const float film_aperture_width,
-                                const float film_aperture_height,
-                                const uint32_t img_width,
-                                const uint32_t img_height,
-                                const fit_resolution_gate &fit_setting,
-                                const float near_clipping_plain,
-                                const float focal_length, float &top,
-                                float &bottom, float &left, float &right) {
-    // Implements a pinhole camera model
-    float film_aspect_ratio = film_aperture_width / film_aperture_height;
-    float device_aspect_ratio = img_width / (float)img_height;
-    top = ((film_aperture_height * inch_to_mm) / 2 / focal_length) *
-          near_clipping_plain;
-    right = ((film_aperture_width * inch_to_mm) / 2 / focal_length) *
-            near_clipping_plain;
-#ifndef NDEBUG
-    float fov = 2 * 180 / M_PI * atan((right / near_clipping_plain));
-    std::cout << "Field of view : " << fov << std::endl;
-#endif
-    float xscale = 1, yscale = 1;
-    switch (fit_setting) {
-    case fit_resolution_gate::Fill:
-        if (device_aspect_ratio < film_aspect_ratio) {
-            xscale = device_aspect_ratio / film_aspect_ratio;
-        } else {
-            yscale = film_aspect_ratio / device_aspect_ratio;
-        }
-        break;
-    case fit_resolution_gate::Overscan:
-        if (device_aspect_ratio < film_aspect_ratio) {
-            yscale = film_aspect_ratio / device_aspect_ratio;
-        } else {
-            xscale = device_aspect_ratio / film_aspect_ratio;
-        }
+class Camera {
+  private:
+    float inch_to_mm = 25.4f;
+    uint32_t img_width, img_height;
+    float film_aperture_width, film_aperture_height;
+    fit_resolution_gate fit_setting;
+    float near_clipping_plain, far_clipping_plain;
+    float focal_length;
+    float top, bottom, left, right;
+    Matrix44f world_to_cam;
+
+  public:
+    Camera() = delete;
+    Camera(uint32_t width, uint32_t height, float fa_w, float fa_h,
+           float z_near, float z_far, float f_length, Matrix44f w2cam)
+        : img_width(width), img_height(height), film_aperture_width(fa_w),
+          film_aperture_height(fa_h), near_clipping_plain(z_near),
+          far_clipping_plain(z_far), focal_length(f_length),
+          world_to_cam(w2cam) {
+        compute_screen_coordinates();
     }
-    right *= xscale;
-    top *= yscale;
-    bottom = -top;
-    left = -right;
-}
-void convert_to_raster(const Vec3f &v_world, const Matrix44f &world_to_cam,
-                       const float t, const float b, const float l,
-                       const float r, const float near, const float img_width,
-                       const float img_height, Vec3f &raster) {
-    Vec3f v_cam;
-    world_to_cam.mult_vec_matrix(v_world, v_cam);
-    // Convert to screen space
-    // Perform perspective divide
-    Vec2f v_screen;
-    v_screen.x = near * v_cam.x / -v_cam.z;
-    v_screen.y = near * v_cam.y / -v_cam.z;
-    // Convert to NDC
-    Vec2f v_NDC;
-    v_NDC.x = 2 * v_screen.x / (r - l) - (r + l) / (r - l);
-    v_NDC.y = 2 * v_screen.y / (t - b) - (t + b) / (t - b);
-    // Convert to raster space
-    raster.x = (v_NDC.x + 1) / 2 * img_width;
-    raster.y = (1 - v_NDC.y) / 2 * img_height;
-    raster.z = -v_cam.z;
-}
-}
+    void compute_screen_coordinates() {
+        // Implements a pinhole camera model
+        float film_aspect_ratio = film_aperture_width / film_aperture_height;
+        float device_aspect_ratio = img_width / (float)img_height;
+        top = ((film_aperture_height * inch_to_mm) / 2 / focal_length) *
+              near_clipping_plain;
+        right = ((film_aperture_width * inch_to_mm) / 2 / focal_length) *
+                near_clipping_plain;
+#ifndef NDEBUG
+        float fov = 2 * 180 / M_PI * atan((right / near_clipping_plain));
+        std::cout << "Field of view : " << fov << std::endl;
+#endif
+        float xscale = 1, yscale = 1;
+        switch (fit_setting) {
+        case fit_resolution_gate::Fill:
+            if (device_aspect_ratio < film_aspect_ratio) {
+                xscale = device_aspect_ratio / film_aspect_ratio;
+            } else {
+                yscale = film_aspect_ratio / device_aspect_ratio;
+            }
+            break;
+        case fit_resolution_gate::Overscan:
+            if (device_aspect_ratio < film_aspect_ratio) {
+                yscale = film_aspect_ratio / device_aspect_ratio;
+            } else {
+                xscale = device_aspect_ratio / film_aspect_ratio;
+            }
+        }
+        right *= xscale;
+        top *= yscale;
+        bottom = -top;
+        left = -right;
+    }
+    void convert_to_raster(const Vec3f &v_world, Vec3f &raster) {
+        Vec3f v_cam;
+        world_to_cam.mult_vec_matrix(v_world, v_cam);
+        // Convert to screen space
+        // Perform perspective divide
+        Vec2f v_screen;
+        v_screen.x = near_clipping_plain * v_cam.x / -v_cam.z;
+        v_screen.y = near_clipping_plain * v_cam.y / -v_cam.z;
+        // Convert to NDC
+        Vec2f v_NDC;
+        v_NDC.x =
+            2 * v_screen.x / (right - left) - (right + left) / (right - left);
+        v_NDC.y =
+            2 * v_screen.y / (top - bottom) - (top + bottom) / (top - bottom);
+        // Convert to raster space
+        raster.x = (v_NDC.x + 1) / 2 * img_width;
+        raster.y = (1 - v_NDC.y) / 2 * img_height;
+        raster.z = -v_cam.z;
+    }
+};
 }
 #endif
--- include/rasteriser_alpha.hpp
+++ include/rasteriser_alpha.hpp
@@ -12,6 +12,7 @@
 //===----------------------------------------------------------------------===//
 #ifndef RASTERISER_ALPHA_HPP
 #define RASTERISER_ALPHA_HPP
+#include <buffers_alpha.hpp>
 #include <camera_alpha.hpp>
 #include <fstream>
 #include <math_alpha.hpp>
@@ -19,95 +20,28 @@
 #include <prettyprint.hpp>
 namespace alpha {
 using namespace math;
-using rgb = Vec3<uint8_t>;
-using Point = Vec2i;
-class Zbuffer {};
-class Framebuffer {
-    std::vector<std::vector<uint8_t>> buffer;
-    uint32_t width, height;
-
-  public:
-    int col_space;
-    Framebuffer() = delete;
-    Framebuffer(uint32_t w, uint32_t h, int space)
-        : width(w), height(h), col_space(space) {
-        buffer.assign(height, {});
-        for (auto &elem : buffer) {
-            elem.assign(3 * width, 0);
-        }
-    }
-    void set(uint32_t x, uint32_t y, uint8_t r, uint8_t g, uint8_t b) {
-        buffer[y][3 * x] = r;
-        buffer[y][3 * x + 1] = g;
-        buffer[y][3 * x + 2] = b;
-    }
-    void dump_as_ppm(const std::string &name) {
-        std::ofstream file_h(name, std::fstream::binary);
-        file_h << "P6 " << width << " " << height << " " << col_space << " ";
-        for (auto &row : buffer) {
-            for (auto elem : row) {
-                file_h << elem;
-            }
-        }
-        file_h.close();
-    }
-};
 class Rasteriser {
-    std::unique_ptr<Framebuffer> buf;
-    inline int edge_function(const Point &a, const Point &b, const Point &c) {
-        return (a.x - b.x) * (c.y - a.y) + (b.y - a.y) * (c.x - a.x);
+    std::unique_ptr<Framebuffer> Ibuf;
+    std::unique_ptr<Zbuffer> Zbuf;
+    std::shared_ptr<Camera> cam;
+    inline float edgeFunction(const Vec3f &a, const Vec3f &b, const Vec3f &c) {
+        return (c[0] - a[0]) * (b[1] - a[1]) - (c[1] - a[1]) * (b[0] - a[0]);
     }
     int screen_width, screen_height;
+    float far_clipping_plain;
 
   public:
     Rasteriser() = delete;
-    Rasteriser(uint32_t w, uint32_t h, int col_space = 255)
+    Rasteriser(uint32_t w, uint32_t h, std::shared_ptr<Camera> cam_inst,
+               int col_space = 255)
         : screen_width(w), screen_height(h) {
-        buf = std::unique_ptr<Framebuffer>(
+        Ibuf = std::unique_ptr<Framebuffer>(
             new Framebuffer(screen_width, screen_height, col_space));
+        Zbuf = std::unique_ptr<Zbuffer>(
+            new Zbuffer(screen_width, screen_height, far_clipping_plain));
+        cam = cam_inst;
     }
-    void dump_as_ppm(const std::string &name) { buf->dump_as_ppm(name); }
-    void draw_triangle(const Point &v0, const Point &v1, const Point &v2) {
-        const int min_x = std::max(min_3(v0.x, v1.x, v2.x), 0.f);
-        const int min_y = std::max(min_3(v0.y, v1.y, v2.y), 0.f);
-        const int max_x =
-            std::min(max_3(v0.x, v1.x, v2.x), float(screen_width - 1));
-        const int max_y =
-            std::min(max_3(v0.y, v1.y, v2.y), float(screen_height - 1));
-        // Keep in screen bounds
-        const float total_area = edge_function(v0, v1, v2);
-        const float mult = buf->col_space / total_area;
-        // Computing the barycentric co-ordinates for colour contribution
-        // Triangle setup
-        // Convention : Anticlockwise winding
-        const int a01 = v0.y - v1.y, b01 = v1.x - v0.x;
-        const int a12 = v1.y - v2.y, b12 = v2.x - v1.x;
-        const int a20 = v2.y - v0.y, b20 = v0.x - v2.x;
-        int w0_row = edge_function(v0, v1, {min_x, min_y});
-        int w1_row = edge_function(v1, v2, {min_x, min_y});
-        int w2_row = edge_function(v2, v0, {min_x, min_y});
-        std::cout << "w's" << a01 << ", " << a12 << ", " << a20 << std::endl;
-        int w0, w1, w2;
-        int x, y;
-        auto &buf_ref = *buf;
-        for (y = min_y; y <= max_y; y++) {
-            w0 = w0_row;
-            w1 = w1_row;
-            w2 = w2_row;
-            for (x = min_x; x <= max_x; x++) {
-                if ((w1 | w2 | w0) >= 0) {
-                    // If visible, render
-                    buf_ref.set(x, y, w0 * mult, w1 * mult, w2 * mult);
-                }
-                w0 += a01;
-                w1 += a12;
-                w2 += a20;
-            }
-            w0_row += b01;
-            w1_row += b12;
-            w2_row += b20;
-        }
-    }
+    void dump_as_ppm(const std::string &name) { Ibuf->dump_as_ppm(name); }
 };
 }
 #endif
