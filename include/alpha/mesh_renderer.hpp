b2b6ef4aff45994bb6de160b73ed5d81cd8647c6
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2020-03-22 19:33:54 +0530

    [#2] Refactor mesh rendering executable to use cxxopts

---
+++ include/alpha/mesh_renderer.hpp
@@ -0,0 +1,67 @@
+//===---- mesh_renderer ----- Vertex mesh renderer --------------*- C++ -*-===//
+//
+// Alpha-trace -> Minimal C++ raytracer
+//
+// Written by: Shikhar Bhardwaj | shikhar@bluefog.me
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Render a vertex mesh to an SVG
+///
+//===----------------------------------------------------------------------===//
+#ifndef ALPHA_MESH_RENDERER
+#define ALPHA_MESH_RENDERER
+
+#include <alpha/camera.hpp>
+#include <alpha/rasteriser.hpp>
+#include <alpha/svg.hpp>
+#include <alpha/vertex_import.hpp>
+
+namespace alpha {
+    struct MeshRenderer {
+        std::string _mesh_data_file;
+        std::string _camera_file;
+        std::string _output_file;
+        bool _cull_back_faces;
+
+        // Camera
+        std::shared_ptr<alpha::Camera> _cam_inst = std::make_shared<alpha::Camera>(_camera_file);
+
+        // Rasteriser
+        alpha::Rasteriser<> _rast = alpha::Rasteriser<>(_cam_inst);
+
+        // SVG Export
+        std::unique_ptr<alpha::SVG_export> _exporter = std::make_unique<alpha::SVG_export>(
+            _cam_inst->img_width, _cam_inst->img_height, _output_file);
+
+        // Mesh data
+        alpha::mesh_data _data = alpha::mesh_data(_mesh_data_file, true);
+
+        MeshRenderer() = delete;
+
+        MeshRenderer(std::string mesh_data_file, std::string camera_file,
+            std::string output_file, bool cull_back_faces) :
+            _mesh_data_file(mesh_data_file), _camera_file(camera_file),
+            _output_file(output_file), _cull_back_faces(cull_back_faces) {}
+
+        void render() {
+            for (uint32_t i = 0; i < _data.num_triangles; ++i) {
+                const auto& v0 = _data.vertices[3 * i];
+                const auto& v1 = _data.vertices[3 * i + 1];
+                const auto& v2 = _data.vertices[3 * i + 2];
+
+                if (!_cull_back_faces || _rast.draw_triangle(v0, v1, v2)) {
+                    alpha::math::Vec3f v0_rast, v1_rast, v2_rast;
+                    _cam_inst->convert_to_raster(v0, v0_rast);
+                    _cam_inst->convert_to_raster(v1, v1_rast);
+                    _cam_inst->convert_to_raster(v2, v2_rast);
+                    _exporter->put_line(v0_rast, v1_rast);
+                    _exporter->put_line(v1_rast, v2_rast);
+                    _exporter->put_line(v2_rast, v0_rast);
+                }
+            }
+        }
+    };
+}
+#endif
--- src/render_mesh.cpp
+++ src/render_mesh.cpp
@@ -6,107 +6,57 @@
 #include <algorithm>
 #include <string>
 
-#include <alpha/camera.hpp>
-#include <alpha/rasteriser.hpp>
-#include <alpha/svg.hpp>
-#include <alpha/vertex_import.hpp>
+#include <cxxopts.hpp>
 
-bool verbose = false;
-bool blender = false;
-void help() {
-    // The command line parameters
-    std::cout << "render_mesh : Render a polygon mesh" << std::endl;
-    std::cout << "-o file_name\t: Name of the exported SVG" << std::endl;
-    std::cout << "-c file_name\t: Name of the camera config file" << std::endl;
-    std::cout << "-m file_name\t: Name of the RAW mesh data file" << std::endl;
-    std::cout << "-v          \t: Be verbose" << std::endl;
-    std::cout << "-b          \t: Pass if the vertex data is Y-up\n";
-    std::cout << "-t          \t: Pass if the camera is column major\n";
-    std::cout << "-z          \t: Pass to hide non-visible faces\n";
-    std::cout << "-h          \t: Display help and exit" << std::endl;
-    std::exit(0);
-}
-void print_info(const std::string &msg) {
-    if (verbose) {
-        std::cout << "INFO : " << msg << std::endl;
+#include <alpha/mesh_renderer.hpp>
+#include <alpha/utils.hpp>
+
+using namespace std;
+
+void check_option(const cxxopts::ParseResult& result, std::string option) {
+    if (result.count(option) == 0) {
+        throw cxxopts::OptionException("Missing required option: " + option);
     }
 }
-std::string get_name(const std::string &switch_val,
-                     const std::vector<std::string> &args) {
-    // Get file name from the switch
-    auto it = std::find(args.begin(), args.end(), switch_val);
-    if (it == args.end()) {
-        std::cerr << "Required parameter : " << switch_val << std::endl;
-        std::exit(1);
-    }
-    if (it + 1 == args.end()) {
-        std::cerr << switch_val << " requires a file name" << std::endl;
-        std::exit(1);
-    }
-    return *(it + 1);
-}
+
 int main(int argc, char **argv) {
-    std::vector<std::string> args(argc);
-    for (int i = 0; i < argc; i++) {
-        args[i] = std::string(argv[i]);
-    }
-    // Now process the args
-    if (std::find(args.begin(), args.end(), "-h") != args.end()) {
-        help();
-    }
-    if (std::find(args.begin(), args.end(), "-v") != args.end()) {
-        verbose = true;
-    }
-    if (std::find(args.begin(), args.end(), "-b") != args.end()) {
-        blender = true;
-    }
-    auto svg_name = get_name("-o", args);
-    auto cam_name = get_name("-c", args);
-    auto vert_name = get_name("-m", args);
-    print_info("Intializing data....");
-    std::shared_ptr<alpha::Camera> cam_inst;
-    if (std::find(args.begin(), args.end(), "-t") != args.end()) {
-        cam_inst = std::make_shared<alpha::Camera>(cam_name, true);
-    } else {
-        cam_inst = std::make_shared<alpha::Camera>(cam_name);
-    }
-    alpha::SVG_export svg(cam_inst->img_width, cam_inst->img_height, svg_name);
-    alpha::mesh_data data(vert_name, blender);
-    print_info("Done.");
-    if (verbose) {
-        cam_inst->print_info();
-        std::cout << "Number of triangles : " << data.num_triangles
-                  << std::endl;
-    }
-    if (std::find(args.begin(), args.end(), "-z") != args.end()) {
-        alpha::Rasteriser<> rast(cam_inst);
-        for (uint32_t i = 0; i < data.num_triangles; i++) {
-            const auto &v0 = data.vertices[3 * i];
-            const auto &v1 = data.vertices[3 * i + 1];
-            const auto &v2 = data.vertices[3 * i + 2];
-            if (rast.draw_triangle(v0, v1, v2)) {
-                alpha::math::Vec3f v0_rast, v1_rast, v2_rast;
-                cam_inst->convert_to_raster(v0, v0_rast);
-                cam_inst->convert_to_raster(v1, v1_rast);
-                cam_inst->convert_to_raster(v2, v2_rast);
-                svg.put_line(v0_rast, v1_rast);
-                svg.put_line(v1_rast, v2_rast);
-                svg.put_line(v2_rast, v0_rast);
-            }
+    // Command line options configuration
+    cxxopts::Options options("render_mesh", "Render a polygon mesh");
+
+    options.add_options()("m,mesh_data", "Path to RAW mesh data file", cxxopts::value <std::string>())
+        ("o,output_file", "Path to output SVG", cxxopts::value<std::string>()->default_value("a.svg"))
+        ("c,camera_config", "Path to Camera configuration", cxxopts::value <std::string>())
+        ("v,verbose", "Enable verbose mode", cxxopts::value<bool>()->default_value("false"))
+        ("z,cull_back_faces","Enable back face culling", cxxopts::value<bool>()->default_value("false"))
+        ("h,help", "Display help and exit");
+
+    // Parse and set options
+    try {
+        auto result = options.parse(argc, argv);
+
+        if (result.count("help") != 0) {
+            // Display help and exit.
+            std::cout << options.help() << std::endl;
+            return 0;
         }
 
-    } else {
-        for (uint32_t i = 0; i < data.num_triangles; i++) {
-            const auto &v0 = data.vertices[3 * i];
-            const auto &v1 = data.vertices[3 * i + 1];
-            const auto &v2 = data.vertices[3 * i + 2];
-            alpha::math::Vec3f v0_rast, v1_rast, v2_rast;
-            cam_inst->convert_to_raster(v0, v0_rast);
-            cam_inst->convert_to_raster(v1, v1_rast);
-            cam_inst->convert_to_raster(v2, v2_rast);
-            svg.put_line(v0_rast, v1_rast);
-            svg.put_line(v1_rast, v2_rast);
-            svg.put_line(v2_rast, v0_rast);
-        }
+        // Check required options
+        check_option(result, "mesh_data");
+        check_option(result, "camera_config");
+
+        // Configure options
+        auto mesh_data_file = result["mesh_data"].as<std::string>();
+        auto camera_config_file = result["camera_config"].as<std::string>();
+        auto output_file = result["output_file"].as<std::string>();
+        auto verbose = result["verbose"].as<bool>();
+        auto cull_back_faces = result["cull_back_faces"].as<bool>();
+
+        (void)verbose;
+
+        // Run the projection
+        alpha::MeshRenderer(mesh_data_file, camera_config_file, output_file, cull_back_faces).render();
+    } catch(const cxxopts::OptionException& e) {
+        alpha::exception_handler(e);
+        std::cout << options.help() << std::endl;
     }
 }
